32138687b1adc422fa0c2773e85dcd0ad1001111
Rafael Martinez <rafaelmartinez.contato@gmail.com>
2024-02-08 19:38:41 -0300

    add a comments session in posts

--- src/components/Blog/Postagens/Posts.vue
+++ src/components/Blog/Postagens/Posts.vue
@@ -1,7 +1,6 @@
 <template>
   <section class="posts">
     <FormCriarPost />
-
     <div class="posts__list" v-for="post in postsFetched" :key="post.id">
       <div v-if="!updatingPost">
         <Post
@@ -20,6 +19,26 @@
         <button @click="saveEditedPost(post)">Salvar</button>
         <button @click="cancelEditPost">Cancelar</button>
       </div>
+      <div v-if="!post.showComments && !updatingPost">
+        <button @click="showComments(post)">
+          Ler Comentários ({{ commentsFetched[post.id]?.length || 0 }})
+        </button>
+      </div>
+      <div v-else-if="post.showComments">
+        <!-- Correção: Renderizar comentários somente se showComments for verdadeiro -->
+        <div class="post__comments" v-if="commentsFetched[post.id]">
+          <button @click="hideComments(post)">Esconder Comentários</button>
+          <Comentario
+            v-for="comment in commentsFetched[post.id]"
+            :key="comment.id"
+            :email="comment.email"
+            :name="comment.name"
+            :body="comment.body"
+            :PostId="comment.postId"
+            :id="comment.id"
+          />
+        </div>
+      </div>
     </div>
   </section>
 </template>
@@ -30,10 +49,14 @@ import * as api from "../../../services/api";
 import FormCriarPost from "../Postagens/FormCriarPost.vue";
 import Post from "../Postagens/Post.vue";
 import DeletarPost from "../Postagens/DeletarPost.vue";
+import Comentario from "../Comentarios/Comentario.vue";
 
 const postsFetched = ref([]);
+const commentsFetched = ref({});
+
 const updatingPost = ref(false);
 const postBeingEdited = ref(null);
+
 const newTitle = ref("");
 const newBody = ref("");
 
@@ -56,6 +79,7 @@ const saveEditedPost = async (post) => {
     postBeingEdited.value = null;
     newTitle.value = "";
     newBody.value = "";
+    console.log("Post Editado com Sucesso");
     await handleFetchPosts();
   } catch (error) {
     console.error(error.message);
@@ -73,9 +97,30 @@ const handleFetchPosts = async () => {
   try {
     const response = await api.getAllPosts();
     postsFetched.value = response.data;
+    for (const post of postsFetched.value) {
+      await handleFetchComments(post.id);
+      post.showComments = false;
+    }
   } catch (error) {
     console.error(error.message);
   }
+};
+
+const handleFetchComments = async (postId) => {
+  try {
+    const response = await api.getCommentsPost(postId);
+    commentsFetched.value[postId] = response.data;
+  } catch (error) {
+    console.error(error.message);
+  }
+};
+
+const showComments = (post) => {
+  post.showComments = true;
+};
+
+const hideComments = (post) => {
+  post.showComments = false;
 };
 
 handleFetchPosts();
