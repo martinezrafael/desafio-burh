17673eb0cc0eddfdd71256a12df5e96c88ad007d
Rafael Martinez <rafaelmartinez.contato@gmail.com>
2024-02-08 09:22:59 -0300

    create a request getAllPosts

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,28 @@
 <template>
   <h1>Desafio Burh | Frontend</h1>
+  <div v-for="post in postsFetched" :key="post.id">
+    <h2>{{ post.title }}</h2>
+    <p>{{ post.body }}</p>
+    <span>{{ post.userId }}</span>
+  </div>
 </template>
-<script setup></script>
+
+<script setup>
+import { ref } from "vue";
+import * as api from "../src/services/api";
+
+const postsFetched = ref([]);
+
+const handleFetchData = async () => {
+  try {
+    const response = await api.getAllPosts();
+    postsFetched.value = response.data;
+  } catch (error) {
+    console.error(error.message);
+  }
+};
+
+handleFetchData();
+</script>
+
 <style scoped></style>
--- src/services/api.js
+++ src/services/api.js
@@ -2,4 +2,15 @@ import axios from "axios";
 
 const api = axios.create({
   baseURL: "https://jsonplaceholder.typicode.com/",
+  headers: {
+    "Content-type": "application/json; charset=UTF-8",
+  },
 });
+
+export const getAllPosts = async () => {
+  try {
+    return await api.get("/posts");
+  } catch (error) {
+    console.error({ message: error.message });
+  }
+};
