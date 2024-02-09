8e1ec7f09c49c9aa0fa49b2bcfb1f13c83f1d6f2
Rafael Martinez <rafaelmartinez.contato@gmail.com>
2024-02-09 14:14:00 -0300

    add styles in header

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,10 @@
 <template>
-  <h1>Blog Burh</h1>
+  <Header :title="'blog'" />
   <Posts />
 </template>
 
 <script setup>
+import Header from "../src/components/Commons/Header.vue";
 import Posts from "../src/components/Blog/Postagens/Posts.vue";
 </script>
 
---
+++ src/components/Commons/Header.vue
@@ -0,0 +1,19 @@
+<template>
+  <header class="header">
+    <div class="header__container">
+      <a class="logo__href" href="/">
+        <h1 class="logo">{{ title }}</h1>
+      </a>
+    </div>
+  </header>
+</template>
+<script setup>
+import { defineProps } from "vue";
+
+const props = defineProps({
+  title: {
+    type: String,
+    required: true,
+  },
+});
+</script>
