80ae8c791b8310aa833800c593e41e5bb598ba43
Rafael Martinez <rafaelmartinez.contato@gmail.com>
2024-02-14 16:01:29 -0300

    transform css FormcriarPost in scss

--- vite.config.js
+++ vite.config.js
@@ -5,11 +5,6 @@ import vue from "@vitejs/plugin-vue";
 
 export default defineConfig({
   plugins: [vue()],
-  css: {
-    preprocessorOptions: {
-      scss: { additionalData: `@import "src/assets/sass/main.scss";` },
-    },
-  },
   resolve: {
     alias: {
       "@": fileURLToPath(new URL("./src", import.meta.url)),
